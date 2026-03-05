f38f222a0a50dee11f1e9c6638d6133b3856ec1f
Miroshnik-Mariya <mariya3027545@gmail.com>
2026-03-05 13:48:34 +0400

    Задание №1
    Добавлена функция перехода на вторую Activity по кнопке.

--- app/src/main/java/com/example/homework1_miroshnik_mariya/SecondActivity.kt
+++ app/src/main/java/com/example/homework1_miroshnik_mariya/SecondActivity.kt
@@ -1,32 +1,20 @@
 package com.example.homework1_miroshnik_mariya
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val textEnter: EditText = findViewById(R.id.editTextEnterText)
-        val openActivity2: Button = findViewById(R.id.btnAct2)
-
-
-        openActivity2.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
         }
     }
 }
