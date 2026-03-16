3fde7b7b3f853ad092c054961d6c0ec2985e1a2c
Miroshnik-Mariya <mariya3027545@gmail.com>
2026-03-16 11:20:45 +0400

    Домашняя работа №3 - рабочая версия
    Полностью переделанное задание №2 - «Создание UI в Android».

---
+++ app/src/main/java/io/mmaltsev/vkeducation/presentation/MainActivity.kt
@@ -0,0 +1,30 @@
+package io.mmaltsev.vkeducation.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
+import com.example.vk.presentation.AppNavHost
+import dagger.hilt.android.AndroidEntryPoint
+import io.mmaltsev.vkeducation.presentation.appdetails.AppDetailsScreen
+import io.mmaltsev.vkeducation.presentation.theme.VkEducationTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VkEducationTheme {
+//                AppDetailsScreen()
+                AppNavHost()
+            }
+        }
+    }
+}
+
+
+
