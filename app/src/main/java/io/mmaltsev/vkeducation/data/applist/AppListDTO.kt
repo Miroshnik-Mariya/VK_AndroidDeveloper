dc638be70b14f57112db5d3469372ab2b633002a
Mariya Miroshnik <mariya3027545@gmail.com>
2026-03-28 16:51:50 +0400

    feat: обновить AppListDTO
    Добавили @SerialName

--- app/src/main/java/io/mmaltsev/vkeducation/data/applist/AppListDTO.kt
+++ app/src/main/java/io/mmaltsev/vkeducation/data/applist/AppListDTO.kt
@@ -1,13 +1,18 @@
 package io.mmaltsev.vkeducation.data.applist
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AppListDto(
+    @SerialName("id")
     val id: Int,
+    @SerialName("name")
     val name: String,
+    @SerialName("category")
     val category: String,
+    @SerialName("description")
     val description: String,
-    val iconRes: Int,
-    val iconUrl: String? = null
+    @SerialName("icon")
+    val iconUrl: String
 )
